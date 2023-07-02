0955d4e51738fe8a5c4859756ce63aa887f9646b
Izayoi <1805148679@qq.com>
2023-07-02 14:53:47 +0800

    用户编辑业务完成

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -14,7 +14,7 @@
         <ElCard style="margin-top: 10px;">
             <ElButton type="primary" @click="addUser">添加用户</ElButton>
             <ElButton type="danger">批量删除</ElButton>
-            <ElTable border style="margin: 10px 0;" :data="userArr">
+            <ElTable v-loading="tableLoading" border style="margin: 10px 0;" :data="userArr">
                 <ElTableColumn type="selection"></ElTableColumn>
                 <ElTableColumn type="index" label="#"></ElTableColumn>
                 <ElTableColumn label="ID" prop="id"></ElTableColumn>
@@ -25,7 +25,7 @@
                 <ElTableColumn label="更新时间" prop="updateTime"></ElTableColumn>
                 <ElTableColumn label="操作" width="300px">
                     <template #="{ row }">
-                        <ElButton size="small" type="primary" icon="User">分配角色</ElButton>
+                        <ElButton size="small" type="primary" icon="User" @click="setRole(row)">分配角色</ElButton>
                         <ElButton size="small" type="warning" icon="Edit" @click="updateUser(row)">编辑</ElButton>
                         <ElButton size="small" type="danger" icon="Delete">删除</ElButton>
                     </template>
@@ -37,7 +37,7 @@
         </ElCard>
         <el-drawer v-model="userEditDrawer" direction="rtl">
             <template #header>
-                <h4>添加用户</h4>
+                <h4>{{ userParams.id ? '更新用户' : '添加用户' }}</h4>
             </template>
             <template #default>
                 <ElForm ref="formRef" :model="userParams" :rules="rules">
@@ -47,7 +47,7 @@
                     <ElFormItem label="用户昵称" prop="name">
                         <ElInput placeholder="请输入用户昵称" v-model="userParams.name"></ElInput>
                     </ElFormItem>
-                    <ElFormItem label="用户密码" prop="password">
+                    <ElFormItem label="用户密码" prop="password" v-if="!userParams.id">
                         <ElInput placeholder="请输入用户密码" v-model="userParams.password"></ElInput>
                     </ElFormItem>
                 </ElForm>
@@ -59,6 +59,33 @@
                 </div>
             </template>
         </el-drawer>
+        <el-drawer v-model="setRoleDrawer" direction="rtl">
+            <template #header>
+                <h4>分配角色用户</h4>
+            </template>
+            <template #default>
+                <ElForm>
+                    <ElFormItem label="用户姓名">
+                        <ElInput v-model="userRoleParams.username" :disabled="true"></ElInput>
+                    </ElFormItem>
+                    <ElFormItem label="职位列表">
+                        <el-checkbox v-model="checkAll" :indeterminate="isIndeterminate"
+                            @change="handleCheckAllChange">全选</el-checkbox>
+                        <el-checkbox-group v-model="userRole" @change="handleCheckedRoleChange">
+                            <el-checkbox v-for="role in roles" :key="role" :label="role">{{
+                                role
+                            }}</el-checkbox>
+                        </el-checkbox-group>
+                    </ElFormItem>
+                </ElForm>
+            </template>
+            <template #footer>
+                <div style="flex: auto">
+                    <el-button @click="setRoleCancel">取消</el-button>
+                    <el-button type="primary" @click="setRoleSave">确认</el-button>
+                </div>
+            </template>
+        </el-drawer>
     </div>
 </template>
 
@@ -66,19 +93,13 @@
 import { ref, onMounted, reactive, nextTick } from 'vue';
 import { reqGetUser, reqAddOrUpdateUser } from '@/api/acl/user';
 import type { User } from '@/api/acl/user/type';
-import { FormRules, FormInstance } from 'element-plus';
+import { FormRules, FormInstance, CheckboxValueType } from 'element-plus';
 
+//#region 分页列表展示相关
+let tableLoading = ref(false)
 let pageNo = ref(1)
 let limit = ref(5)
 let total = ref(0)
-let userArr = ref<User[]>([])
-let userEditDrawer = ref(false)
-let userParams = reactive<User>({
-    username: '',
-    name: '',
-    password: ''
-})
-let formRef = ref<FormInstance>()
 
 onMounted(() => {
     getHasUser()
@@ -90,7 +111,9 @@ onMounted(() => {
  */
 const getHasUser = async (page = 1) => {
     pageNo.value = page
+    tableLoading.value = true
     let result = await reqGetUser(pageNo.value, limit.value)
+    tableLoading.value = false
     if (result.code === 200) {
         userArr.value = result.data.records
         total.value = result.data.total
@@ -105,6 +128,17 @@ const sizeChange = (pageSize: number) => {
     limit.value = pageSize
     getHasUser()
 }
+//#endregion 分页列表展示相关
+
+//#region 编辑添加用户相关
+let userArr = ref<User[]>([])
+let userEditDrawer = ref(false)
+let userParams = reactive<User>({
+    username: '',
+    name: '',
+    password: ''
+})
+let formRef = ref<FormInstance>()
 
 /**
  * 添加用户按钮回调
@@ -113,13 +147,16 @@ const addUser = () => {
     userEditDrawer.value = true
     //重置用户表单数据
     Object.assign(userParams, {
+        id: undefined,
         username: '',
         name: '',
         password: ''
     })
 
     nextTick(() => {
-        formRef.value!.resetFields()
+        formRef.value!.clearValidate('username')
+        formRef.value!.clearValidate('name')
+        formRef.value!.clearValidate('password')
     })
 }
 
@@ -129,8 +166,14 @@ const addUser = () => {
  */
 const updateUser = (row: User) => {
     userEditDrawer.value = true
-}
+    Object.assign(userParams, row)
 
+    nextTick(() => {
+        formRef.value!.clearValidate('username')
+        formRef.value!.clearValidate('name')
+        formRef.value!.clearValidate('password')
+    })
+}
 
 /**
  * 取消按钮回调
@@ -149,11 +192,13 @@ const save = async () => {
     if (!re) return
 
     let result = await reqAddOrUpdateUser(userParams)
+
+    let msg = userParams.id ? '更新' : '添加'
     if (result.code === 200) {
-        ElMessage.success('添加成功')
-        getHasUser()
+        ElMessage.success(`${msg}成功!`)
+        getHasUser(userParams.id ? pageNo.value : 1)
     } else {
-        ElMessage.error('添加失败：' + `${result.message}|${result.data}`)
+        ElMessage.error(`${msg}失败:${result.message}|${result.data}`)
     }
     userEditDrawer.value = false
 }
@@ -170,16 +215,63 @@ const validatorPassword = (_rule: any, value: string, callback: any) => {
 //表单校验规则
 const rules = reactive<FormRules>({
     username: [
-        { required: true, min: 5, max: 10, message: '用户名长度应为5-10位', trigger: 'blur' },
+        { required: true, min: 5, max: 20, message: '用户名长度应为5-20位', trigger: 'blur' },
     ],
     name: [
-        { required: true, min: 5, max: 10, message: '昵称长度应为5-10位', trigger: 'blur' },
+        { required: true, min: 5, max: 20, message: '昵称长度应为5-20位', trigger: 'blur' },
     ],
     password: [
         // { required: true, min: 6, max: 15, message: '密码长度应为6-15位', trigger: 'blur' },
         { required: true, trigger: 'change', validator: validatorPassword }
     ]
 })
+//#endregion 编辑添加用户相关
+
+//#region 分配角色相关
+let userRoleParams = reactive({
+    username: '',
+    role: []
+})
+let setRoleDrawer = ref(false)
+const checkAll = ref(false)
+const isIndeterminate = ref(true)
+const userRole = ref(['Shanghai', 'Beijing'])
+const roles = ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen']
+
+const handleCheckAllChange = (val: CheckboxValueType) => {
+    userRole.value = val ? roles : []
+    isIndeterminate.value = false
+}
+
+const handleCheckedRoleChange = (val: CheckboxValueType[]) => {
+    const checkedCount = val.length
+    checkAll.value = checkedCount === roles.length
+    isIndeterminate.value = checkedCount > 0 && checkedCount < roles.length
+}
+
+/**
+ * 分配角色按钮回调
+ * @param row 
+ */
+const setRole = (row: User) => {
+    setRoleDrawer.value = true
+    userRoleParams.username = row.username
+}
+
+/**
+ * 分配角色取消按钮回调
+ */
+const setRoleCancel = () => {
+
+}
+
+/**
+ * 分配角色确认按钮回调
+ */
+const setRoleSave = () => {
+
+}
+//#endregion 分配角色相关
 </script>
 
 <style scoped lang="scss">
