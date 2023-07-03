2913aed2a456c4516c40d473c595a993d8445a22
Izayoi <1805148679@qq.com>
2023-07-03 17:03:10 +0800

    -

--- components.d.ts
+++ components.d.ts
@@ -41,6 +41,7 @@ declare module 'vue' {
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTag: typeof import('element-plus/es')['ElTag']
+    ElTree: typeof import('element-plus/es')['ElTree']
     ElUpload: typeof import('element-plus/es')['ElUpload']
     Pagination: typeof import('./src/components/Pagination/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
