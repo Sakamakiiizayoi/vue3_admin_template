badd835ad90f34afb14f9db170f27a976b1986db
Izayoi <1805148679@qq.com>
2023-07-06 16:25:11 +0800

    ts类型修改

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -1,26 +1,26 @@
 <template>
     <template v-for="(item, _index) in data" :key="item.path">
         <!-- 没有children -->
-        <ElMenuItem v-if="!item.children && !item.meta.hidden" :index="item.path">
-            <ElIcon v-if="item.meta.icon">
-                <component :is="item.meta.icon"></component>
+        <ElMenuItem v-if="!item.children && !item.meta!.hidden" :index="item.path">
+            <ElIcon v-if="item.meta!.icon">
+                <component :is="item.meta!.icon"></component>
             </ElIcon>
-            <span>{{ item.meta.title }}</span>
+            <span>{{ item.meta!.title }}</span>
         </ElMenuItem>
         <!-- 有children但是只有一个 -->
-        <ElMenuItem v-if="item.children && item.children.length === 1 && !item.meta.hidden" :index="item.children[0].path">
-            <ElIcon v-if="item.children[0].meta.icon">
-                <component :is="item.children[0].meta.icon"></component>
+        <ElMenuItem v-if="item.children && item.children.length === 1 && !item.meta!.hidden" :index="item.children[0].path">
+            <ElIcon v-if="item.children[0].meta!.icon">
+                <component :is="item.children[0].meta!.icon"></component>
             </ElIcon>
-            <span>{{ item.children[0].meta.title }}</span>
+            <span>{{ item.children[0].meta!.title }}</span>
         </ElMenuItem>
         <!-- 有children且大于1 -->
-        <ElSubMenu v-if="item.children && item.children.length > 1 && !item.meta.hidden" :index="item.path">
+        <ElSubMenu v-if="item.children && item.children.length > 1 && !item.meta!.hidden" :index="item.path">
             <template #title>
-                <ElIcon v-if="item.meta.icon">
-                    <component :is="item.meta.icon"></component>
+                <ElIcon v-if="item.meta!.icon">
+                    <component :is="item.meta!.icon"></component>
                 </ElIcon>
-                <span>{{ item.meta.title }}</span>
+                <span>{{ item.meta!.title }}</span>
             </template>
             <Menu :menuList="item.children"></Menu>
         </ElSubMenu>
@@ -28,10 +28,10 @@
 </template>
 
 <script setup lang="ts">
-import { ConstantRoute } from '@/router/routes';
+import type { RouteRecordRaw } from 'vue-router';
 
 const props = defineProps(['menuList'])
-let data: typeof ConstantRoute = props.menuList
+let data: RouteRecordRaw[] = props.menuList
 
 </script>
 <script lang="ts">
