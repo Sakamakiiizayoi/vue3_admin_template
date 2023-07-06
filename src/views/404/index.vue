3c2f9f35009063545a3c08ef84cc4110631d5a27
Izayoi <1805148679@qq.com>
2023-07-06 16:23:45 +0800

    404页面搭建

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        404
+    <div class="container">
+        <svg-icon name="404" width="500px" height="500px"></svg-icon>
+        <div class="des">
+            <ElText class="tip">未找到此页面或无权查看</ElText>
+            <div style="margin-top: 10px;">
+                <el-link type="primary" @click="router.push({ name: 'Home' })">返回首页</el-link>
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
-
+import { useRouter } from 'vue-router';
+let router = useRouter()
 </script>
-<style lang="">
-    
+<style lang="scss">
+.container {
+    display: flex;
+    justify-content: center;
+
+    .des {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        .tip {
+            font-size: 26px;
+            font-weight: 600;
+        }
+    }
+}
 </style>
